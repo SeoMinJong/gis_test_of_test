6618012af3e9b3964b493f7da6d7bc99f6be9838
SeoMinJong <tjalswhd113@naver.com>
2021-10-08 15:49:28 +0900

    10/08 gis_test_of_test 받기만 할 것

--- gis_test_of_test/settings/deploy.py
+++ gis_test_of_test/settings/deploy.py
@@ -1,25 +1,15 @@
 from .base import *
 
 
-env_list = dict()
+def read_secret(secret_name):
+    file = open('/run/secrets/'+ secret_name)
+    secret = file.read()
+    secret = secret.lstrip().rstrip()
+    file.close()
+    return secret
 
-local_env = open(os.path.join(BASE_DIR, '.env'))
 
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\\n', '')  # 한줄로
-    start = line.find('=')  # = 이 있는 곳을 찾기
-    key = line[:start]  # 왼쪽부분을 키  SECRET_KEY = 'django-insecure-ez14bcuso741!155ys$8h1v6kl-gs$ok-)8k56ll*6jz!cr404'
-    value = line[start + 1:]  # 오른쪽을 밸류
-    env_list[key] = value  # 반복문으로 라인을 부르고 파일의 끝에 도달하면 라인이 아니므로 멈춤
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$k_dl(kdp$tx3#mog9kxvqc4$!lknt00+kyqdbjh1l9)@6l+%9'
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -34,9 +24,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'password1234',
+        'USER': read_secret('MARIADB_USER'),
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
-        'PORT': '3306',
+        'PORT': '5432',
     }
 }
